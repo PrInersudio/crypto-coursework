bd6bb6bc585097366119fbfd8a821b867e8d661f
Семин Владислав <sminvlad@rambler.ru>
2024-05-26 05:12:23 +0300

    Добавлен DiffSet

--- DiffSet.py
+++ DiffSet.py
@@ -1,8 +1,8 @@
 from SM4 import T, bytes_xor
-a2 = 0xf3000030
+a2 = 0xf3000030.to_bytes(4)
 DiffSet = set()
 for x in range(1<<32):
     x = x.to_bytes(4)
-    set.add(bytes_xor(T(x),T(bytes_xor(x,a2))))
+    DiffSet.add(bytes_xor(T(x),T(bytes_xor(x,a2))))
 with open("DiffSet.txt", "w") as f:
     print(DiffSet, file=f)
